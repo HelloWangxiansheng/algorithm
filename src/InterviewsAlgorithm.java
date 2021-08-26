d9250aec311a61de58e13ebb733c41858f52fc33
wangwangwang <wangwangwang@kuaishou.com>
2021-08-26 18:42:04 +0800

    字节跳动面试算法

--- src/InterviewsAlgorithm.java
+++ src/InterviewsAlgorithm.java
@@ -7,17 +7,53 @@ import java.util.Scanner;
 public class InterviewsAlgorithm {
 
     /**
-     * 面试笔试遇到的算法
+     * 面试笔试遇到的算法真题
      * @param args
      */
 
+    private static int allNums=0;
     public static void main(String[] args) {
-
-        //输入一个整数，转换成16进制，并判断是否为回文数，不是返回0，是返回1
+        //shoppee
+        /**
+         * 输入一个整数，转换成16进制，并判断是否为回文数，不是返回0，是返回1
+         */
 //        Scanner scanner = new Scanner(System.in);
 //        int i = scanner.nextInt();
 //        Integer2Hex(i);
+
+        //字节跳动 来自jinxin的面试反馈
+        /**
+         * 有0 1 2 3 4 5 6 7 8 9 十个数字，构成一个环，9在0的旁边
+         * 先有个人在0这个位置出发，走出n步后回到原点的走法有多少种？ 只能一步一步走
+         * n=16
+         */
+
+        getNumbersWalker(0,0,16);
+
+        System.out.println(allNums*2);//*2是因为当前的方法只考虑了向右，没考虑向左
+
     }
+
+    /**
+     *
+     * @param nowIndex 当前索引位置
+     * @param walk_step 已步行的步数
+     * @param step 总共可以走的步数
+     * @return
+     */
+    private static void getNumbersWalker(int nowIndex, int walk_step, int step) {
+        if(walk_step==step&&nowIndex==0){
+            allNums++;
+            return;
+        }
+        if(walk_step==step){
+            return;
+        }
+        getNumbersWalker(nowIndex+1,walk_step+1,step);
+        getNumbersWalker((nowIndex-1+10)%10,walk_step+1,step);
+
+    }
+
     //输入一个整数，转换成16进制，并判断是否为回文数，不是返回0，是返回1
     private static void Integer2Hex(int i) {
         int num=i;
