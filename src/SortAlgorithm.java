9430a0c98d6ecab9d6acfb72fbd9a2c37d3f560b
wangwangwang <wangwangwang@kuaishou.com>
2021-08-31 17:50:19 +0800

    归并排序，堆排序

--- src/SortAlgorithm.java
+++ src/SortAlgorithm.java
@@ -1,5 +1,6 @@
 import java.sql.SQLOutput;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 /**
  * @author wangwangwang <wangwangwang@kuaishou.com>
@@ -9,10 +10,18 @@ public class SortAlgorithm {
 
     public static void main(String[] args) {
         int[] arr = {5, 9, 1, 9, 5, 3, 7, 6, 1};
-        Arrays.stream(arr).forEach(e-> System.out.print(" "+e));
-        quickSort(arr,0,arr.length-1);
-        System.out.println(">>");
+//        Arrays.stream(arr).forEach(e-> System.out.print(" "+e));
+        //快排
+//        quickSort(arr,0,arr.length-1);
+        //堆排序
+//        heapSort(arr);
+//        System.out.println(">>");
 //        Arrays.stream(arr).sorted().forEach(e-> System.out.println(e));
+//        PriorityQueue p =new PriorityQueue();
+
+        //归并排序
+//        arr = mergeSort(arr,0,arr.length-1);
+
         Arrays.stream(arr).forEach(e-> System.out.print(" "+e));
     }
 
@@ -43,6 +52,93 @@ public class SortAlgorithm {
         }
         if (i-1>start) quickSort(arr,start,i-1);
         if (j+1<end) quickSort(arr,j+1,end);
+    }
+
+    /**
+     * 堆排序
+     */
+
+    private static void heapSort(int [] array){
+        for (int i = array.length / 2 - 1; i >= 0; i--) {
+            adjustHeap(array, i, array.length);  //调整堆
+        }
+
+        // 上述逻辑，建堆结束
+        // 下面，开始排序逻辑
+        for (int j = array.length - 1; j > 0; j--) {
+            // 元素交换,作用是去掉大顶堆
+            // 把大顶堆的根元素，放到数组的最后；换句话说，就是每一次的堆调整之后，都会有一个元素到达自己的最终位置
+            swap(array, 0, j);
+            // 元素交换之后，毫无疑问，最后一个元素无需再考虑排序问题了。
+            // 接下来我们需要排序的，就是已经去掉了部分元素的堆了，这也是为什么此方法放在循环里的原因
+            // 而这里，实质上是自上而下，自左向右进行调整的
+            adjustHeap(array, 0, j);
+        }
+    }
+
+    /**
+     * 整个堆排序最关键的地方
+     * @param array 待组堆
+     * @param i 起始结点
+     * @param length 堆的长度
+     */
+    public static void adjustHeap(int[] array, int i, int length) {
+        // 先把当前元素取出来，因为当前元素可能要一直移动
+        int temp = array[i];
+        for (int k = 2 * i + 1; k < length; k = 2 * k + 1) {  //2*i+1为左子树i的左子树(因为i是从0开始的),2*k+1为k的左子树
+            // 让k先指向子节点中最大的节点
+            if (k + 1 < length && array[k] < array[k + 1]) {  //如果有右子树,并且右子树大于左子树
+                k++;
+            }
+            //如果发现结点(左右子结点)大于根结点，则进行值的交换
+            if (array[k] > temp) {
+                swap(array, i, k);
+                // 如果子节点更换了，那么，以子节点为根的子树会受到影响,所以，循环对子节点所在的树继续进行判断
+                i  =  k;
+            } else {  //不用交换，直接终止循环
+                break;
+            }
+        }
+    }
+
+    /**
+     * 交换元素
+     * @param arr
+     * @param a 元素的下标
+     * @param b 元素的下标
+     */
+    public static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+
+    /**
+     * 归并排序
+     * @param nums
+     * @param l
+     * @param h
+     * @return
+     */
+    public static int[] mergeSort(int[] nums, int l, int h) {
+        if (l == h)
+            return new int[] { nums[l] };
+
+        int mid = l + (h - l) / 2;
+        int[] leftArr = mergeSort(nums, l, mid); //左有序数组
+        int[] rightArr = mergeSort(nums, mid + 1, h); //右有序数组
+        int[] newNum = new int[leftArr.length + rightArr.length]; //新有序数组
+
+        int m = 0, i = 0, j = 0;
+        while (i < leftArr.length && j < rightArr.length) {
+            newNum[m++] = leftArr[i] < rightArr[j] ? leftArr[i++] : rightArr[j++];
+        }
+        while (i < leftArr.length)
+            newNum[m++] = leftArr[i++];
+        while (j < rightArr.length)
+            newNum[m++] = rightArr[j++];
+        return newNum;
     }
 
 }
---
+++ src/testTemp.java
@@ -0,0 +1,36 @@
+/**
+ * @author wangwangwang <wangwangwang@kuaishou.com>
+ * Created on 2021-08-31
+ */
+
+/**
+ * test alrorithm
+ * 练习
+ */
+
+public class testTemp {
+
+    public static void main(String[] args) {
+        int n=50;//多少行
+        int[][] arr =new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                if(j==0){
+                    arr[i][j]=1;
+                    System.out.print(1);
+                    continue;
+                }else if(j==i){
+                    arr[i][j]=1;
+                    System.out.print(" "+1);
+                    continue;
+                }
+                if(i>1&&j>=1){
+                    int num=arr[i-1][j]+arr[i-1][j-1];
+                    arr[i][j]=num;
+                    System.out.print(" "+num);
+                }
+            }
+            System.out.println();
+        }
+    }
+}
